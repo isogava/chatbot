ea732aa3cbb556f0b432ed1558c96ac5b47f471b
isogava <118534092+isogava@users.noreply.github.com>
2022-12-20 22:00:22 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,12 @@ import streamlit as st
 st.title('チャットボットのテスト')
 st.caption('キャプション')
 
+name = ""
+if name == "";
+
+else;
+  st.text(f'ようこそ！{name}さん！')
+
+
 name = st.text_input('コメント')
 print(name)
-
-st.text(f'ようこそ！{name}さん！')
