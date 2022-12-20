420e315803cc55996eef16d913e2ba850b08d614
isogava <118534092+isogava@users.noreply.github.com>
2022-12-21 00:21:36 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,9 +14,7 @@ text = st.text_input("ご質問をどうぞ")
 if 'text_list' not in st.session_state:
   st.session_state["text_list"] = []
 
-if len(st.text_input) == 0:
-        
-else:
+if not st.text_input:
     st.session_state["text_list"].append(text)
     st.session_state["text_list"].append(text + "なんですね。")
         
