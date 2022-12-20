f7239467dbf7cc2717105c2a08639331b5b60587
isogava <118534092+isogava@users.noreply.github.com>
2022-12-20 22:38:40 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,15 +9,18 @@ st.caption('キャプション')
 #else:
 #  st.text(f'ようこそ！{name}さん！')
 
+list = []
 dict = {'こんにちは':'コンニチハ','おはよう':'早起きですね！',
         'こんばんは':'コンバンハ',
         'はい':'ハイ'}
 name = st.text_input('コメントをどうぞ！')
 
-if name == "aaa":
+if name == "スズキ":
   st.text(f'ようこそ！{name}さん！')
+  list = list.append(name)
 elif name in dict:
   st.text('私の辞書では、' + dict[name] + 'と返事をします。')
+  list = list.append(name)
 else:
   if name == '':
     st.text('コメントをお願いします')
@@ -28,3 +31,4 @@ c = 5
 while c > 1:
         st.text('もっと話して')
         c = c - 1
+st.text(list)
