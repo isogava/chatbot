b87b6cc338859bdc46e2dab4731fd95f3b44777c
isogava <118534092+isogava@users.noreply.github.com>
2022-12-20 22:15:36 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,10 +9,11 @@ st.caption('キャプション')
 #else:
 #  st.text(f'ようこそ！{name}さん！')
 
-counter = 5
-print(counter)
+
 name = st.text_input('コメント')
 
 st.text(f'ようこそ！{name}さん！')
 
+name = name, name*2 
+st.text(f'ようこそ！{name}さん！')
 #st.text(f'こんばんは！{name}さん！')
