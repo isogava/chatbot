3204ff3912747667c160989e46100832579bee97
isogava <118534092+isogava@users.noreply.github.com>
2022-12-20 23:06:13 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,16 +13,17 @@ list = ['はじめまして！']
 dict = {'こんにちは':'コンニチハ','おはよう':'早起きですね！',
         'こんばんは':'コンバンハ',
         'はい':'ハイ'}
-name = st.text_input('コメントをどうぞ！')
+while True:
+        name = st.text_input('コメントをどうぞ！')
 
-if name == "スズキ":
-  st.text(f'ようこそ！{name}さん！')
-  list.append(name)
-elif name in dict:
-  st.text('私の辞書では、' + dict[name] + 'と返事をします。')
-  list.append(name)
-else:
-  if len(name) > 0:
-    list.append(name)
+        if name == "スズキ":
+          st.text(f'ようこそ！{name}さん！')
+          list.append(name)
+        elif name in dict:
+          st.text('私の辞書では、' + dict[name] + 'と返事をします。')
+          list.append(name)
+        else:
+          if len(name) > 0:
+            list.append(name)
 
-st.text(list)
+        st.text(list)
