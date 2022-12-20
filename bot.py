ec0ae82dfbf3a1da2ba37e38bfb8d02d59fedb80
isogava <118534092+isogava@users.noreply.github.com>
2022-12-21 00:15:24 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,17 +14,11 @@ text = st.text_input("ご質問をどうぞ")
 if 'text_list' not in st.session_state:
   st.session_state["text_list"] = []
 
-col1, col2 = st.columns(2)
-
-with col2:
-  if st.text_input:
+if st.text_input:
     st.session_state["text_list"].append(text)
-with col1:
-  if st.text_input:
-    st.write(text,"desu")
       
 for output_text in st.session_state["text_list"]:
-  st.write('<span style="color:red;background:pink">', output_text, '</span>')
+  st.write(output_text)
 
 # 風船飛ばす
 st.balloons()
