86f4dc6be43b8c77dc7eb9dde424ad0934203efc
isogava <118534092+isogava@users.noreply.github.com>
2022-12-21 00:31:39 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,20 +7,22 @@ list = ['はじめまして！']
 dict = {'こんにちは':'コンニチハ','おはよう':'早起きですね！',
         'こんばんは':'コンバンハ',
         'はい':'ハイ'}
+col1, col2 = st.colums((1,1))
 
-
-text = st.text_input("ご質問をどうぞ")
+with col1:
+  text = st.text_input("ご質問をどうぞ")
 
 if 'text_list' not in st.session_state:
   st.session_state["text_list"] = []
 
-if st.text_input:
-  if len(text) > 0:
-    st.session_state["text_list"].append(text)
-    st.session_state["text_list"].append(text + "なんですね。")
-        
-for output_text in st.session_state["text_list"]:
-  st.write(output_text)
+with col2:
+        if st.text_input:
+          if len(text) > 0:
+            st.session_state["text_list"].append(text)
+            st.session_state["text_list"].append(text + "なんですね。")
+
+        for output_text in st.session_state["text_list"]:
+          st.write(output_text)
 
 # 風船飛ばす
 st.balloons()
